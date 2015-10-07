c5224466ccab4fdde512a80502084d6bd6a739d4
Aldo <agordillos@gmail.com>
2015-10-07 16:46:25 +0200

    Basic user session management with Devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < Devise::RegistrationsController
+  
+  protected
+
+  def after_sign_up_path_for(resource)
+    view_context.home_path
+  end
+
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
+  
+end
---
+++ db/migrate/20151007135210_add_user_fields.rb
@@ -0,0 +1,9 @@
+class AddUserFields < ActiveRecord::Migration
+  def up
+    add_column :users, :name, :string
+  end
+
+  def down
+    remove_column :users, :name
+  end
+end
